a9abe7b61fcdca582b8c5789cf7ffa7270e61a26
Oron Ben Zvi <oronbz@gmail.com>
2023-03-03 22:36:32 +0200

    Initial Commit

---
+++ TCAOverrideDependencySingleMethod/TCAOverrideDependencySingleMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAOverrideDependencySingleMethodApp.swift
+//  TCAOverrideDependencySingleMethod
+//
+//  Created by Oron Ben Zvi on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAOverrideDependencySingleMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
